c787c016b40a5fff3faa5cb71c9bcbf6a1a7f5a4
= <mertkucukhuseyin90@gmail.com>
2026-08-20 16:39:34 +0200

    Fixed printing.

--- src/render/render.c
+++ src/render/render.c
@@ -3,8 +3,13 @@
 #include "render.h"
 #include "config.h"
 
-void render() {
-    printf("%s%sCherries Deploy%s ───────────────────────────────────── v0.1.0 ──── \n\n", BOLD, RED, RESET);
+void render(Status s) {
+    printf("%s%sCherries Deploy%s ───────────────────────────────────── v0.1.0 ──── \n", BOLD, RED, RESET);
+    printf("%s PID %-16d ─────────────────────────────────────────── %s\n", BOLD, s.pid, RESET);
+    printf("%s STATUS %-13d ─────────────────────────────────────────── %s\n", BOLD, s.status, RESET);
+    printf("%s HASH %-15s ─────────────────────────────────────────── %s\n", BOLD, s.hash, RESET);
+    printf("%s───────────────────────────────────────────────────────────────── %s\n", BOLD, RESET);
+    printf("%s EVENTS: ──────────────────────────────────────────────────────── %s\n", BOLD, RESET);
 }
 
 void renderStatus(Deploy d, Status s) {}
