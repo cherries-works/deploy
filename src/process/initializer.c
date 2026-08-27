3857d598f70e64c805354616996dfacbd0a43af7
= <mertkucukhuseyin90@gmail.com>
2026-08-27 22:57:34 +0200

    SHM is being used (#4).

--- src/process/initializer.c
+++ src/process/initializer.c
@@ -9,21 +9,20 @@
 #include <malloc.h>
 #include <sys/mman.h>
 #include <poll.h>
+#include <fcntl.h>
 
 #include "config.h"
 #include "process.h"
 #include "utils.h"
-#include "setup.h"
 #include "render.h"
 #include "log.h"
+#include "setup.h"
 
 pid_t startProgram(
     Deploy *deploy,
     Status *status,
     Args *args
 ) {
-    setup();
-
     status->status = parsing;
     parseConfig(args->config, deploy);
     if(!deploy->exists) return -1;
@@ -43,67 +42,81 @@ pid_t startProgram(
     return program_pid;
 }
 
-pid_t initialize(
-    Status *status,
-    Args *args
-) {
+pid_t initialize(Args *args) {
     Deploy deploy = {};
     pid_t main_pid = fork();
     if(main_pid == -1) return -1;
-
+    
     if(main_pid == 0) {
+        int shm_fd = shm_open(CHERRIES_DEPLOY_SHM, O_RDWR, 0600);
+        if (shm_fd == -1) {
+            printf("SHM FD -1.\n");
+            exit(EXIT_FAILURE);
+        }
+    
+        ftruncate(shm_fd, sizeof(struct shmbuf));
+        struct shmbuf *shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if (shmp == MAP_FAILED) {
+            printf("Mapping object.\n");
+            _log(
+                L_ERROR,
+                "Mapping object failed"
+            );
+            exit(EXIT_FAILURE);
+        }
+
         _log(L_INFO, "Setting up Deploy.");
 
-        pid_t program_pid = startProgram(&deploy, status, args);
+        pid_t program_pid = startProgram(&deploy, &shmp->status, args);
         if(program_pid == -1) return -1;
         
         char previous_head[BUFFER_ONE_KB];
         char head[BUFFER_ONE_KB];
 
-        status->status = waiting;
+        shmp->status.status = waiting;
         char message[64];
         while(true) {
             int pid_status;
             pid_t r = waitpid(program_pid, &pid_status, WNOHANG);
-            strcpy(status->hash, deploy.head);
+            strcpy(shmp->status.hash, deploy.head);
 
             if (r == program_pid) {
-                status->latest_commit_check = false;
-                status->failed_commit_check = false;
+                shmp->status.latest_commit_check = false;
+                shmp->status.failed_commit_check = false;
                 if (WIFEXITED(pid_status)) {
-                    eventAppend(status, "x Application failed (exited)\n");
+                    eventAppend(&shmp->status, "x Application failed (exited)\n");
                 } else if (WIFSIGNALED(pid_status)) {
-                    eventAppend(status, "x Application terminated (terminated)\n");
+                    eventAppend(&shmp->status, "x Application terminated (terminated)\n");
                 } else {
-                    eventAppend(status, "x Application failed (restarting)\n");
+                    eventAppend(&shmp->status, "x Application failed (restarting)\n");
                 }
 
-                eventAppend(status, "^ Restarting\n");
-                status->status = building;
+                eventAppend(&shmp->status, "^ Restarting\n");
+                shmp->status.status = building;
 
-                program_pid = restart(deploy, status);
+                program_pid = restart(deploy, &shmp->status);
                 if(program_pid == -1) {
-                    eventAppend(status, "x Application failed (exited)\n");
+                    eventAppend(&shmp->status, "x Application failed (exited)\n");
                     exit(EXIT_FAILURE);
                 }
 
                 snprintf(message, sizeof(message), "+ Application restarted (pid %d)", program_pid);
-                eventAppend(status, message);
-                status->status = idle;
+                eventAppend(&shmp->status, message);
+                shmp->status.status = idle;
             }
 
             parseHead(deploy, head);
             if(strcmp(deploy.head, head) != 0 && deploy.upgrade) {
                 if(strcmp(deploy.failed_head, head) == false) {
-                    if(status->failed_commit_check == false) {
-                        status->failed_commit_check = true;
+                    if(shmp->status.failed_commit_check == false) {
+                        shmp->status.failed_commit_check = true;
                     }
                 } else {
-                    status->status = building;
+                    shmp->status.status = building;
     
-                    status->latest_commit_check = false;
-                    eventAppend(status, "^ New commit detected");
-                    eventAppend(status, "^ Updating program");
+                    shmp->status.latest_commit_check = false;
+                    eventAppend(&shmp->status, "^ New commit detected");
+                    eventAppend(&shmp->status, "^ Updating program");
     
                     if(strlen(deploy.previous_head) > 0) {
                         strcpy(previous_head, deploy.previous_head);
@@ -115,26 +128,26 @@ pid_t initialize(
                     stop(program_pid);
     
                     snprintf(message, sizeof(message), "+ Stopped application (pid %d)", program_pid);
-                    eventAppend(status, message);
-                    program_pid = restart(deploy, status);
+                    eventAppend(&shmp->status, message);
+                    program_pid = restart(deploy, &shmp->status);
     
                     if(program_pid == -1) {
-                        eventAppend(status, "x Application failed (rolling back)");
+                        eventAppend(&shmp->status, "x Application failed (rolling back)");
                         program_pid = rollback(&deploy, deploy.previous_head);
                         if(program_pid == -1) {
-                            eventAppend(status, "x Application failed (exited)");
+                            eventAppend(&shmp->status, "x Application failed (exited)");
                             exit(EXIT_FAILURE);
                         } else {
-                            eventAppend(status, "+ Rollback completed");
-                            status->status = deploying;
+                            eventAppend(&shmp->status, "+ Rollback completed");
+                            shmp->status.status = deploying;
                         }
                     }
     
-                    status->failed_commit_check = false;
+                    shmp->status.failed_commit_check = false;
     
                     snprintf(message, sizeof(message), "+ Application running (pid %d)", program_pid);
-                    eventAppend(status, message);
-                    status->status = deploying;
+                    eventAppend(&shmp->status, message);
+                    shmp->status.status = deploying;
     
                     if(deploy.prune && strlen(previous_head) != 0) {
                         char previous_head_path[BUFFER_ONE_KB];
@@ -144,12 +157,12 @@ pid_t initialize(
                 }
             }
     
-            if(!status->latest_commit_check) {
-                status->latest_commit_check = true;
-                eventAppend(status, "^ Waiting for next commit");
+            if(!shmp->status.latest_commit_check) {
+                shmp->status.latest_commit_check = true;
+                eventAppend(&shmp->status, "^ Waiting for next commit");
             }
     
-            status->status = idle;
+            shmp->status.status = idle;
             sleep(deploy.wait);
         }
     }
--- src/render/render.c
+++ src/render/render.c
@@ -6,11 +6,14 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 #include "render.h"
 #include "config.h"
 #include "utils.h"
+#include "process.h"
+#include "log.h"
 
 const char* status_list[] = {
     "idle",
@@ -22,46 +25,62 @@ const char* status_list[] = {
     "deploying"
 };
 
-void render(Status *s) {
+void render() {
     pid_t pid = fork();
     if (pid < 0) {
         printf("Error: Forking renderer failed.\n");
         return;
     }
 
+    
     if(pid == 0) {
-        while(true) {
-            clearLines((unsigned)s->previous_terminal_length);
+        int shm_fd = shm_open(CHERRIES_DEPLOY_SHM, O_RDWR, 0600);
+        if (shm_fd == -1) {
+            exit(EXIT_FAILURE);
+        }
+    
+        ftruncate(shm_fd, sizeof(struct shmbuf));
+        struct shmbuf *shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if (shmp == MAP_FAILED) {
+            _log(
+                L_ERROR,
+                "Mapping object failed"
+            );
+            exit(EXIT_FAILURE);
+        }
+        shmp->status.render_pid = getpid();
 
-            s->previous_terminal_length = 0;
+        while(true) {
+            clearLines((unsigned)shmp->status.previous_terminal_length);
+
+            shmp->status.previous_terminal_length = 0;
             printf("%s%sCherries Deploy%s ──────────────────────────── v0.2.0 ──────┐\n", BOLD, RED, RESET);
             printf("Press 'd' to detach | Press 'q' to quit\n\n");
             
-            s->previous_terminal_length += 3;
+            shmp->status.previous_terminal_length += 3;
 
             printf("%s┌─────────────────────────────────────────────────────────┐ %s\n", BOLD, RESET);
-            printf("%s│ PID %-51d │%s\n", BOLD, s->pid, RESET);
-            printf("%s│ STATUS %-48s │%s\n", BOLD, status_list[s->status], RESET);
-            printf("%s│ HASH %-50s │%s\n", BOLD, s->hash, RESET);
+            printf("%s│ PID %-51d │%s\n", BOLD, shmp->status.pid, RESET);
+            printf("%s│ STATUS %-48s │%s\n", BOLD, status_list[shmp->status.status], RESET);
+            printf("%s│ HASH %-50s │%s\n", BOLD, shmp->status.hash, RESET);
             
             char formatted_time[256];
-            formatTimeHumanReadable((unsigned)s->timer, formatted_time, 256);
+            formatTimeHumanReadable((unsigned)shmp->status.timer, formatted_time, 256);
             printf("%s│ TIME %-50s │%s\n", BOLD, formatted_time, RESET);
             printf("%s└─────────────────────────────────────────────────────────┘ %s\n", BOLD, RESET);
             printf("%s┌─────────────────────────────────────────────────────────┐ %s\n", BOLD, RESET);
             printf("%s│ EVENTS: %-52s│\n", BOLD, RESET);
 
-            s->previous_terminal_length += 8;
+            shmp->status.previous_terminal_length += 8;
 
-            for(int i = 0; i < s->event_index; i++) {
-                printf("%s│ %-56s│%s\n", BOLD, s->events[i], RESET);
-                s->previous_terminal_length++;
+            for(int i = 0; i < shmp->status.event_index; i++) {
+                printf("%s│ %-56s│%s\n", BOLD, shmp->status.events[i], RESET);
+                shmp->status.previous_terminal_length++;
             }
             printf("%s└─────────────────────────────────────────────────────────┘ %s\n", BOLD, RESET);
-            s->previous_terminal_length++;
-            s->timer++;
+            shmp->status.previous_terminal_length++;
+            shmp->status.timer++;
             sleep(1);
         }
     }
-    s->render_pid = pid;
 }
