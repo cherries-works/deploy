09e037ee2f7c1ed4c6889b26680370caf5c3a71a
= <mertkucukhuseyin90@gmail.com>
2026-08-27 22:58:20 +0200

    Deleted files.

--- src/commands/help.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "render.h"
-
-void help() {
-    printf("%s%sCherries Deploy%s ───────────────────────────────────── v0.2.0 ──── \n", BOLD, RED, RESET);
-    printf("     %-20s %-20s\n", "--config [path]", "Specify the location of config file.");
-    printf("     %-20s %-20s\n\n", "--help", "Prints this.");
-}
